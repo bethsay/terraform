950e900e413c991cf10bf759548f03f632e8eecc
Bethsay Tom V <bethsay.tv@gmail.com>
2026-02-17 21:57:04 +0530

    Changed on trigger to workflow_dispatch

--- create-s3-bucket/providers.tf
+++ create-s3-bucket/providers.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   shared_credentials_files = ["~/.aws/credentials"]
   shared_config_files      = ["~/.aws/config"]
-  profile                  = "default"
+  # profile                  = "default"
   default_tags {
     tags = {
       project = "training",
